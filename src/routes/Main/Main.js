e37101d7155d0cf8d0f23dc2b6fe08dd6914821e
zoeferencova <zoeferencova@gmail.com>
2020-03-26 19:55:50 -0400

    added project photos

--- src/routes/Main/Main.js
+++ src/routes/Main/Main.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Kocolabs from '../../images/kocolabs.png'
+import Coordinator from '../../images/coordinatortool.png'
+import TravelBuddy from '../../images/travelbuddy.png'
 import styles from './Main.module.css'
 import shared from '../../utils/Shared.module.css'
 
 export default class Main extends Component {
     render() {
         return (
-            <div>
-                
+            <div className={styles.container}>
+                <Link to="/coordinator" className={styles.coordinator} ><img src={Coordinator} height="350px"></img></Link>
+                <Link to="/kocolabs" className={styles.kocolabs}><img src={Kocolabs} height="250px"></img></Link>
+                <Link to="/travelbuddy" className={styles.travelbuddy}><img src={TravelBuddy} height="375px"></img></Link>
             </div>
         );
     }
