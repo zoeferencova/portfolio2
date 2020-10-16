2b76c1feb758824d5b5774ccfa14ba13ac9a0eb9
zoeferencova <zoeferencova@gmail.com>
2020-10-16 16:23:55 -0400

    Updates

---
+++ precache-manifest.cc5b8bf9ebb5047236f3e6d423f0d8f7.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ec1fd01afe7e7a83740916df4703e0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b2f4059ae0dd56a40da1",
+    "url": "/portfolio/static/css/main.c4d6538e.chunk.css"
+  },
+  {
+    "revision": "539a27381f5330bd8a91",
+    "url": "/portfolio/static/js/2.591ba10e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.591ba10e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f4059ae0dd56a40da1",
+    "url": "/portfolio/static/js/main.d12dcc28.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f48a7fd292d6bd65fc245abcbd579892",
+    "url": "/portfolio/static/media/cats.f48a7fd2.JPG"
+  },
+  {
+    "revision": "809ca7a539a0118fcfa08e0bd5d0fef7",
+    "url": "/portfolio/static/media/community-chat.809ca7a5.png"
+  },
+  {
+    "revision": "4a58f62f39996685ab999bee5efd200a",
+    "url": "/portfolio/static/media/community-main.4a58f62f.png"
+  },
+  {
+    "revision": "a3d692c6b334dee296c1f6d8403997c3",
+    "url": "/portfolio/static/media/community-post.a3d692c6.png"
+  },
+  {
+    "revision": "7e8fbe427c5bd66c65c8cf807656490f",
+    "url": "/portfolio/static/media/community.7e8fbe42.png"
+  },
+  {
+    "revision": "88ae1e143892fb8b28ac279193233379",
+    "url": "/portfolio/static/media/coordinator-account.88ae1e14.png"
+  },
+  {
+    "revision": "672d6f3bb4546a52a617aaf206182516",
+    "url": "/portfolio/static/media/coordinator-dashboard.672d6f3b.png"
+  },
+  {
+    "revision": "886d4d66cefd0e5be605893ad060ee23",
+    "url": "/portfolio/static/media/coordinator-main.886d4d66.png"
+  },
+  {
+    "revision": "9f3f6562293f7a9d13f8a4828fb7ea06",
+    "url": "/portfolio/static/media/coordinator-templates.9f3f6562.png"
+  },
+  {
+    "revision": "a9a11c44e9438e1189e51ef8f7645ab9",
+    "url": "/portfolio/static/media/coordinator.a9a11c44.png"
+  },
+  {
+    "revision": "f6bbac50c027181fba876c3f338ac455",
+    "url": "/portfolio/static/media/headshot.f6bbac50.JPG"
+  },
+  {
+    "revision": "593476ce3a8fc4f71e3f410249ea0b51",
+    "url": "/portfolio/static/media/kocolabs-collection.593476ce.png"
+  },
+  {
+    "revision": "d1e88848381a43fa0cd2144c8bf50776",
+    "url": "/portfolio/static/media/kocolabs-main.d1e88848.png"
+  },
+  {
+    "revision": "87a3beae7b957ae01db1d99f272e479c",
+    "url": "/portfolio/static/media/kocolabs-product.87a3beae.png"
+  },
+  {
+    "revision": "3ba1acfe5eba2162c54106516368de5e",
+    "url": "/portfolio/static/media/kocolabs.3ba1acfe.png"
+  },
+  {
+    "revision": "797f69d7f6579d5664b93610fb9ac6ef",
+    "url": "/portfolio/static/media/travelbuddy-info.797f69d7.png"
+  },
+  {
+    "revision": "fb37b4b469914855101d65e0d67120a1",
+    "url": "/portfolio/static/media/travelbuddy-main.fb37b4b4.png"
+  },
+  {
+    "revision": "a3422d7692fb591be8f1ae31c314155a",
+    "url": "/portfolio/static/media/travelbuddy.a3422d76.png"
+  }
+]);
